41ceddaf321ac760c492645f26668aaa28e8d5e9
JiSeon <xoxqqq@naver.com>
2022-04-13 03:26:35 +0900

    promise

---
+++ app/src/main/java/com/example/promise/After_Login.java
@@ -0,0 +1,14 @@
+package com.example.promise;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class After_Login extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_after_login);
+    }
+}
--- app/src/main/java/com/example/promise/Kakao_Login_Activity.java
+++ app/src/main/java/com/example/promise/Kakao_Login_Activity.java
@@ -2,9 +2,11 @@ package com.example.promise;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.util.Log;
 import android.view.View;
+import android.widget.Button;
 import android.widget.ImageView;
 import android.widget.TextView;
 
@@ -18,7 +20,7 @@ import kotlin.jvm.functions.Function1;
 import kotlin.jvm.functions.Function2;
 
 public class Kakao_Login_Activity extends AppCompatActivity {
-    private static final String TAG = "Kakao_Login_Activity";
+    private static final String TAG = "LoginActivity";
     private View loginButton, logoutButton;
     private TextView nickName;
     private ImageView profileImage;
@@ -28,10 +30,19 @@ public class Kakao_Login_Activity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_kakao_login);
 
-        loginButton = findViewById(R.id.login_button);
-        logoutButton = findViewById(R.id.logout);
+        loginButton = findViewById(R.id.login);
+
         nickName = findViewById(R.id.nickname);
-        profileImage = findViewById(R.id.profile);
+
+        Button btn = (Button) findViewById(R.id.button5);
+        btn.setOnClickListener(new View.OnClickListener(){
+            @Override
+            public void onClick(View view){
+                Intent intent = new Intent(getApplicationContext(), MainActivity.class);
+                startActivity(intent);
+            }
+        });
+
 
         // 카카오가 설치되어 있는지 확인 하는 메서드또한 카카오에서 제공 콜백 객체를 이용함
         Function2<OAuthToken, Throwable, Unit> callback = new  Function2<OAuthToken, Throwable, Unit>() {
@@ -39,17 +50,16 @@ public class Kakao_Login_Activity extends AppCompatActivity {
             public Unit invoke(OAuthToken oAuthToken, Throwable throwable) {
                 // 이때 토큰이 전달이 되면 로그인이 성공한 것이고 토큰이 전달되지 않았다면 로그인 실패
                 if(oAuthToken != null) {
-                    Log.i("user", oAuthToken.getAccessToken() + " " + oAuthToken.getRefreshToken());
+
                 }
                 if (throwable != null) {
-                    Log.w(TAG, "invoke: " + throwable.getLocalizedMessage());
+
                 }
                 updateKakaoLoginUi();
                 return null;
             }
         };
         // 로그인 버튼
-
         loginButton.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
@@ -60,19 +70,7 @@ public class Kakao_Login_Activity extends AppCompatActivity {
                 }
             }
         });
-        // 로그 아웃 버튼
-        logoutButton.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                UserApiClient.getInstance().logout(new Function1<Throwable, Unit>() {
-                    @Override
-                    public Unit invoke(Throwable throwable) {
-                        updateKakaoLoginUi();
-                        return null;
-                    }
-                });
-            }
-        });
+
         updateKakaoLoginUi();
     }
     private  void updateKakaoLoginUi(){
@@ -93,24 +91,23 @@ public class Kakao_Login_Activity extends AppCompatActivity {
                     // 유저의 어카운트 정보에 나이
                     Log.d(TAG,"invoke: age" + user.getKakaoAccount().getAgeRange());
 
-                    nickName.setText(user.getKakaoAccount().getProfile().getNickname());
+                    nickName.setText(user.getKakaoAccount().getProfile().getNickname()+"님 환영합니다!");
 
 
                     loginButton.setVisibility(View.GONE);
-                    logoutButton.setVisibility(View.VISIBLE);
+
                 }else {
                     // 로그인이 되어 있지 않다면 위와 반대로
                     nickName.setText(null);
-                    profileImage.setImageBitmap(null);
+
                     loginButton.setVisibility(View.VISIBLE);
-                    logoutButton.setVisibility(View.GONE);
+
                 }
                 return null;
             }
         });
     }
 }
-
 
 
 
