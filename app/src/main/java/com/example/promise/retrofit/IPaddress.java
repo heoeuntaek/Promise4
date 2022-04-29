7b57bc0078361894f662980dd19b5d76f3675755
heoeuntaek <gjdmsxor@naver.com>
2022-04-29 22:46:43 +0900

    은택

--- app/src/main/java/com/example/promise/retrofit/IPaddress.java
+++ app/src/main/java/com/example/promise/retrofit/IPaddress.java
@@ -2,5 +2,6 @@ package com.example.promise.retrofit;
 
 public class IPaddress {
     public final static String IPADRESS = "http://192.168.83.64:8080/";
+//    aws ip주소 - "http://3.34.97.79:8080:8080/"
 
 }
